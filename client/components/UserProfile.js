91ec5a4b924c9c63bf64b484ae7357896b01821f
Kenneth Lai <knthslai@Kenneths-MacBook-Pro-2.local>
2018-07-25 16:49:52 -0400

    user profile

--- client/components/UserProfile.js
+++ client/components/UserProfile.js
@@ -5,15 +5,15 @@ export default class GrassList extends Component {
   render() {
     return (
 
-      <div className="card">
-        <img src="img.jpg" alt="John" style="width:100%" />
-        <h1>John Doe</h1>
-        <p className="title">CEO & Founder, Example</p>
-        <p>Harvard University</p>
-        <a href="#"><i className="fa fa-dribbble" /></a>
-        <a href="#"><i className="fa fa-twitter" /></a>
-        <a href="#"><i className="fa fa-linkedin" /></a>
-        <a href="#"><i className="fa fa-facebook" /></a>
+      <div>
+        <img src="img.jpg" />
+        <h1>Name</h1>
+        <a>E-Mail: </a>
+        <p>Address:</p>
+        <a>Street: </a>
+        <a>City: </a>
+        <a>State: </a>
+        <a>ZipCode: </a>
         <p><button>Update</button></p>
       </div>
     )
