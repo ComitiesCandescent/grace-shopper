e41d699dfb673b598c60634f1cdfdaaa274f361c
Fig Yogurt <itisaftab@gmail.com>
2018-07-25 16:57:49 -0400

    still seed file

--- seed.js
+++ seed.js
@@ -1,7 +1,17 @@
-const {db} = require('./server/db');
+const {db} = require('./server/db')
 const Product = require('./server/db/models/product')
 
 const grasses = [
-    {name: 
-    }
+  {
+    name: 'Grasp Grass',
+    price: 2.5,
+    imageUrl:
+      'https://previews.123rf.com/images/nataliafedorenko/nataliafedorenko0905/nataliafedorenko090500053/4867643-a-male-hand-with-its-fingers-picking-up-the-grass-.jpg'
+  },
+  {
+    name: 'Dead grass',
+    prie: 1,
+    imageUrl:
+      'https://forums.unrealengine.com/filedata/fetch?id=1211751&d=1487743042'
+  }
 ]
