51495b1189fc6e65fc088ac8a9543bfe58d6ec6d
Hadley Callaway <hccallaway@gmail.com>
2018-07-25 17:17:04 -0400

    working on navbar

---
+++ client/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import GrassList from './GrassList'
+import Navbar from './Navbar'
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <GrassList />
+    </div>
+  )
+}
+
+export default Home
