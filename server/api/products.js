f1f84a71f74b40ca4d30592c70375d20a4efdbc0
Hadley Callaway <hccallaway@gmail.com>
2018-07-26 11:23:39 -0400

    basic redux store up for products and users

---
+++ server/api/products.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const router = express.Router()
+const Product = require('../db/models/product')
+
+// /api/products
+router.get('/', async (req, res, next) => {
+  try {
+    res.json(await Product.findAll())
+  } catch (error) {
+    next(error)
+  }
+})
+
+// /api/products/:productId
+router.get('/:productId', async (req, res, next) => {
+  try {
+    res.json(await Product.findById(req.params.productId))
+  } catch (error) {
+    next(error)
+  }
+})
+
+module.exports = router
