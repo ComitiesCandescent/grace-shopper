ba0f42f9b4e1370b19e6226db6607580b92f8de6
Hadley Callaway <hccallaway@gmail.com>
2018-07-27 14:53:41 -0400

    We've started to implement the cart but are running into some issues.

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -1,29 +1,82 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
+import {fetchCartProducts} from '../store/cart'
+import {Card, Image} from 'semantic-ui-react'
+import {NavLink} from 'react-router-dom'
+
+function twoDecimals(price) {
+  return price.toFixed(2)
+}
 
 class Cart extends Component {
+  constructor() {
+    super()
+    this.componentDidMount = this.componentDidMount.bind(this)
+  }
+
   componentDidMount() {
-    console.log('mounted')
-    // this.props.loadProducts()
+    this.props.loadCartProducts()
   }
 
   render() {
-    return <h1>Cart</h1>
+    const products = this.props.products
+    return (
+      <div className="ui items">
+        <h1>Cart</h1>
+        {products.length ? (
+          products.map(product => {
+            return (
+              <div className="item" key={product.id}>
+                <div className="ui small image">
+                  <img src={product.imageUrl} />
+                </div>
+                <div className="content">
+                  <div className="header">{product.name}</div>
+                  <div className="meta">
+                    <span className="price">$1200</span>
+                    <span className="stay">1 Month</span>
+                  </div>
+                  <div className="description">
+                    <p />
+                  </div>
+                </div>
+              </div>
+
+              // <Card key={product.id}>
+              //   <Image src={product.imageUrl} />
+              //   <Card.Content>
+              //     <NavLink to={`/${product.id}`} activeClassName="active">
+              //       <Card.Header>{product.name}</Card.Header>
+              //     </NavLink>
+              //     <Card.Description>{product.description}</Card.Description>
+              //   </Card.Content>
+              //   <Card.Content extra>
+              //     <i className="dollar sign icon">
+              //       {twoDecimals(product.price)}
+              //     </i>
+              //   </Card.Content>
+              // </Card>
+            )
+          })
+        ) : (
+          <h2>No products in cart yet</h2>
+        )}
+      </div>
+    )
   }
 }
 
 const mapStateToProps = state => {
   return {
-    // singleCampus: campus,
-    // enrolledStudents: enrolledStudents
+    products: state.cartState.products
   }
 }
 
 const mapDispatchToProps = (dispatch, ownProps) => {
   return {
-    // loadCampus: () => {
-    //   dispatch(fetchSingleCampus(ownProps.match.params.campusId))
-    // }
+    loadCartProducts: () => {
+      dispatch(fetchCartProducts(ownProps.match.params.userId))
+    }
   }
 }
 
--- client/components/ProductCard.js
+++ client/components/ProductCard.js
@@ -1,54 +1,85 @@
 import React from 'react'
+import {connect} from 'react-redux'
 import {NavLink} from 'react-router-dom'
+import {fetchCartProducts, fetchProductToAdd} from '../store/cart'
 import {Card, Image, Button} from 'semantic-ui-react'
 
 function twoDecimals(price) {
   return price.toFixed(2)
 }
 
-const ProductCard = props => {
-  const product = props.product
-  return (
-    <Card>
-      <Image src={product.imageUrl} />
-      <Card.Content>
-        <NavLink to={`/${product.id}`} activeClassName="active">
-          <Card.Header>{product.name}</Card.Header>
-        </NavLink>
+class ProductCard extends React.Component {
+  constructor() {
+    super()
+    this.componentDidMount = this.componentDidMount.bind(this)
+    this.onClick = this.onClick.bind(this)
+  }
 
-        <Card.Meta>
-          <span className="date">Number of reviews or stars here??</span>
-        </Card.Meta>
-        <Card.Description>{product.description}</Card.Description>
-      </Card.Content>
-      <Card.Content extra>
-        <i className="dollar sign icon">{twoDecimals(product.price)}</i>
-      </Card.Content>
-      <Card.Content extra>
-        <div className="ui vertical animated button" tabIndex="0">
-          <div className="hidden content">Add</div>
-          <div className="visible content">
-            <i className="shop icon" />
+  componentDidMount() {
+    console.log('FIX USER ID LATER!!!!!: ')
+    this.props.loadCartProducts(1)
+  }
+
+  onClick(productId) {
+    this.props.loadProduct(productId)
+  }
+
+  render() {
+    const product = this.props.product
+    return (
+      <Card>
+        <Image src={product.imageUrl} />
+        <Card.Content>
+          <NavLink to={`/${product.id}`} activeClassName="active">
+            <Card.Header>{product.name}</Card.Header>
+          </NavLink>
+          <Card.Meta>
+            <div className="extra">
+              Rating:
+              <div className="ui star rating" data-rating="3" />
+            </div>
+          </Card.Meta>
+          <Card.Description>{product.description}</Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+          <i className="dollar sign icon">{twoDecimals(product.price)}</i>
+        </Card.Content>
+        <Card.Content extra>
+          <div className="ui vertical animated button" tabIndex="0">
+            <button
+              type="button"
+              className="ui button active"
+              onClick={() => {
+                this.onClick(product.id)
+              }}
+            >
+              <div className="hidden content">Add</div>
+              <div className="visible content">
+                <i className="shop icon" />
+              </div>
+            </button>
           </div>
-        </div>
-      </Card.Content>
-    </Card>
-
-    // <div className="singleProduct">
-    //   <div>
-    //     <NavLink to={`/api/products/${product.id}`} activeClassName="active">
-    //       <h3>{product.name}</h3>
-    //     </NavLink>
-    //     <img src={product.imageUrl} />
-    //     <h4>{product.description}</h4>
-    //     <h4>${product.price}</h4>
-    //   </div>
-    //   <div>
-    //     <select>{/* quantity options */}</select>
-    //     <button type="button">Add To Cart</button>
-    //   </div>
-    // </div>
-  )
+        </Card.Content>
+      </Card>
+    )
+  }
 }
 
-export default ProductCard
+const mapStateToProps = state => {
+  return {
+    products: state.cartState.products
+  }
+}
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    loadCartProducts: userId => {
+      dispatch(fetchCartProducts(userId))
+    },
+    loadProduct: productId => {
+      dispatch(fetchProductToAdd(productId))
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProductCard)
